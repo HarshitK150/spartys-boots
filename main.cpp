8891b7dae38c50e9205ee612d3db1dae79048c67
Harshit Kandpal <hkandpal944@gmail.com>
2024-12-31 12:57:50 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file main.cpp
+ * @authors Nitish Maindoliya,
+ */
+
+#include "pch.h"
+#include "Project1App.h"
+
+/// The main entry point for the application
+wxIMPLEMENT_APP(Project1App);
